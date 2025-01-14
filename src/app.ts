c14a3a493b8939e2b3beab7be8a5cf0e536104e4
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2025-01-14 13:15:34 +0600

    global error and route error controller add

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
-import express, { Request, Response } from 'express'
-const app = express()
+import express, { Request, Response } from "express";
+import globalError from "./ErrorHandlers/GlobalError";
+import { routeError } from "./ErrorHandlers/RouteError";
+const app = express();
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('SiSCOTEk server!')
-})
+//parsers
+app.use(express.json());
 
-export default app
+app.get("/", async (req: Request, res: Response, next) => {
+  try {
+    res.send("SiSCOTEk server is running!");
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+app.use("*", routeError);
+app.use(globalError);
+export default app;
