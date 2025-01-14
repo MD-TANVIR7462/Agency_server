042aa12ed1bdd4f30bc4645a1a10efb6d6c05c67
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2025-01-14 12:30:11 +0600

    basic setup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req:Request, res:Response) => {
+  res.send('SiSCOTEk server!')
 })
 
 export default app
