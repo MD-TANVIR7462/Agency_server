bf3946277e27f7cbac17fe5bda80451d6901a0ce
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2025-01-14 12:09:02 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import { Server } from "http";
+import app from "./app";
+const PORT = 5000;
+
+async function main() {
+  const server: Server = app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+  });
+}
+main();
