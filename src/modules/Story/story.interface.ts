5147e7b47913d8b4e0a11a1002004425e83e0017
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2025-01-15 17:28:33 +0600

    story modules working

---
+++ src/modules/Story/story.interface.ts
@@ -0,0 +1,21 @@
+export type TStory = {
+    stats: {
+      years: string;
+      projects: string;
+      teamSize: string;
+      satisfaction: string;
+    };
+    story: {
+      title: string;
+      content: string;
+    };
+    mission: {
+      title: string;
+      content: string;
+    };
+    vision: {
+      title: string;
+      content: string;
+    };
+  };
+  
