7027848a5e93c23188cd6f85a2576a022199f66d
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2025-01-16 10:33:56 +0600

    story validation and model complete

---
+++ src/modules/Story/story.model.ts
@@ -0,0 +1,89 @@
+import { Schema, model } from "mongoose";
+
+const storySchema = new Schema(
+  {
+    stats: {
+      years: {
+        type: String,
+        required: [true, "Years are required."],
+        trim: true,
+        minlength: [1, "Years must have at least 1 character."],
+        maxlength: [4, "Years cannot exceed 4 characters."],
+      },
+      projects: {
+        type: String,
+        required: [true, "Projects are required."],
+        trim: true,
+        minlength: [1, "Years must have at least 1 character."],
+        maxlength: [4, "Years cannot exceed 4 characters."],
+      },
+      teamSize: {
+        type: String,
+        required: [true, "Team size is required."],
+        trim: true,
+        minlength: [1, "Years must have at least 1 character."],
+        maxlength: [4, "Years cannot exceed 4 characters."],
+      },
+      satisfaction: {
+        type: String,
+        required: [true, "Satisfaction percentage is required."],
+        trim: true,
+        minlength: [1, "Years must have at least 1 character."],
+        maxlength: [4, "Years cannot exceed 4 characters."],
+      },
+    },
+    story: {
+      title: {
+        type: String,
+        required: [true, "Story title is required."],
+        trim: true,
+        minlength: [7, "Title must have at least 7 character."],
+        maxlength: [15, "Title cannot exceed 15 characters."],
+      },
+      content: {
+        type: String,
+        required: [true, "Story content is required."],
+        trim: true,
+        minlength: [90, "Title must have at least 90 character."],
+        maxlength: [200, "Content cannot exceed 200 characters."],
+      },
+    },
+    mission: {
+      title: {
+        type: String,
+        required: [true, "Mission title is required."],
+        trim: true,
+        minlength: [7, "Title must have at least 7 character."],
+        maxlength: [15, "Title cannot exceed 15 characters."],
+      },
+      content: {
+        type: String,
+        required: [true, "Mission content is required."],
+        trim: true,
+        minlength: [90, "Title must have at least 90 character."],
+        maxlength: [200, "Content cannot exceed 200 characters."],
+      },
+    },
+    vision: {
+      title: {
+        type: String,
+        required: [true, "Vision title is required."],
+        trim: true,
+        minlength: [7, "Title must have at least 7 character."],
+        maxlength: [15, "Title cannot exceed 15 characters."],
+      },
+      content: {
+        type: String,
+        required: [true, "Vision content is required."],
+        trim: true,
+        minlength: [90, "Years must have at least 90 character."],
+        maxlength: [200, "Content cannot exceed 200 characters."],
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const StoryModel = model("Story", storySchema);
+
+export default StoryModel;
---
+++ src/modules/Story/story.validation.ts
@@ -0,0 +1,66 @@
+import { z } from "zod";
+
+export const validateStory = z
+  .object({
+    stats: z.object({
+      years: z
+        .string()
+        .trim()
+        .min(1, "Years must have at least 1 character.")
+        .max(4, "Years cannot exceed 4 characters."),
+      projects: z
+        .string()
+        .trim()
+        .min(1, "Projects must have at least 1 character.")
+        .max(4, "Projects cannot exceed 4 characters."),
+      teamSize: z
+        .string()
+        .trim()
+        .min(1, "Team size must have at least 1 character.")
+        .max(4, "Team size cannot exceed 4 characters."),
+      satisfaction: z
+        .string()
+        .trim()
+        .min(1, "Satisfaction must have at least 1 character.")
+        .max(4, "Satisfaction cannot exceed 4 characters."),
+    }),
+    story: z.object({
+      title: z
+        .string()
+        .trim()
+        .min(7, "Title must have at least 7 characters.")
+        .max(15, "Title cannot exceed 15 characters."),
+      content: z
+        .string()
+        .trim()
+        .min(90, "Content must have at least 90 characters.")
+        .max(200, "Content cannot exceed 200 characters."),
+    }),
+    mission: z.object({
+      title: z
+        .string()
+        .trim()
+        .min(7, "Title must have at least 7 characters.")
+        .max(15, "Title cannot exceed 15 characters."),
+      content: z
+        .string()
+        .trim()
+        .min(90, "Content must have at least 90 characters.")
+        .max(200, "Content cannot exceed 200 characters."),
+    }),
+    vision: z.object({
+      title: z
+        .string()
+        .trim()
+        .min(7, "Title must have at least 7 characters.")
+        .max(15, "Title cannot exceed 15 characters."),
+      content: z
+        .string()
+        .trim()
+        .min(90, "Content must have at least 90 characters.")
+        .max(200, "Content cannot exceed 200 characters."),
+    }),
+  })
+  .strict();
+
+export const validateUpdateStory = validateStory.partial();
