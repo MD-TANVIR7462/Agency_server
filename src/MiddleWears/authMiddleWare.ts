44499abbfb5463e62af844070cb27bb0be88340d
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2025-03-10 12:11:23 +0600

    auth middleware more secure

--- src/MiddleWears/authMiddleWare.ts
+++ src/MiddleWears/authMiddleWare.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { envConfig } from "../utils/config";
 import { TUserRole } from "../Constant";
+import { RegistrationModel } from "../modules/Auth/Registration/auth.model";
 export const authMiddleware = (...requiredRoles: TUserRole[]) => {
   return (req: Request, res: Response, next: NextFunction): void => {
     try {
@@ -13,9 +14,8 @@ export const authMiddleware = (...requiredRoles: TUserRole[]) => {
         });
         return;
       }
-
       //check if the token is valid
-      jwt.verify(accessToken, envConfig.jwtSecret as string, function (err, decoded) {
+      jwt.verify(accessToken, envConfig.jwtSecret as string, async function (err, decoded) {
         if (err) {
           res.status(401).json({
             success: false,
@@ -23,6 +23,26 @@ export const authMiddleware = (...requiredRoles: TUserRole[]) => {
           });
           return;
         }
+        const isUserExist = await RegistrationModel.findOne({ email: (decoded as JwtPayload).email });
+
+        if (!isUserExist) {
+          res.status(401).json({
+            success: false,
+            message: "Access denied! User not found with this email .",
+          });
+          return;
+        }
+
+        const userActice = isUserExist.isActive;
+
+        if (!userActice) {
+          res.status(401).json({
+            success: false,
+            message: "Access denied! This account exists but currently inactive.",
+          });
+          return;
+        }
+
         //role matching
         if (requiredRoles && !requiredRoles.includes((decoded as JwtPayload)?.role)) {
           res.status(401).json({
@@ -38,5 +58,4 @@ export const authMiddleware = (...requiredRoles: TUserRole[]) => {
       next(error);
     }
   };
-  
 };
--- src/modules/Auth/Registration/auth.routes.ts
+++ src/modules/Auth/Registration/auth.routes.ts
@@ -1,11 +1,12 @@
 // routes/auth.routes.ts
 import { Router } from "express";
 import { AuthController } from "./auth.controller";
+import { permission } from "../../../utils";
 
 const router = Router();
 
 router.post("/", AuthController.registerUser);
-router.get("/all", AuthController.getAllUsers);
+router.get("/all",permission.bothAdmins, AuthController.getAllUsers);
 router.get("/:id", AuthController.getUserById);
 router.patch("/update-user/:id", AuthController.updateUser);
 router.delete("/delete-user/:id", AuthController.deleteUser);
